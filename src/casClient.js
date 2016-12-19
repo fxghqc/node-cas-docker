4856226bd25bd58651fbf4b2e6e384e91310d80e
fxghqc <konxiao@gmail.com>
2016-12-19 20:04:21 +0800

    ignore webpack react router splitted codes

--- src/casClient.js
+++ src/casClient.js
@@ -7,31 +7,32 @@ const SERVER_PATH = process.env.SERVER_PATH;
 
 module.exports = new ConnectCas({
   debug: true,
-    ignore: [
-      /\/ignore/
-    ],
-    match: [],
-    servicePrefix: SERVICE_PREFIX,
-    serverPath: SERVER_PATH,
-    paths: {
-      validate: '/cas/validate',
-      serviceValidate: '/cas/serviceValidate',
-      // proxy: '/cas/proxy',
-      login: '/cas/login',
-      logout: '/cas/logout',
-      proxyCallback: ''
-    },
-    redirect: false,
-    gateway: false,
-    renew: false,
-    slo: true,
-    cache: {
-      enable: false,
-      ttl: 5 * 60 * 1000,
-      filter: []
-    },
-    fromAjax: {
-      header: 'x-client-ajax',
-      status: 418
-    }
+  ignore: [
+    /\/ignore/,
+    /^.*\.js$/  // ignore webpack react router splitted codes 
+  ],
+  match: [],
+  servicePrefix: SERVICE_PREFIX,
+  serverPath: SERVER_PATH,
+  paths: {
+    validate: '/cas/validate',
+    serviceValidate: '/cas/serviceValidate',
+    // proxy: '/cas/proxy',
+    login: '/cas/login',
+    logout: '/cas/logout',
+    proxyCallback: ''
+  },
+  redirect: false,
+  gateway: false,
+  renew: false,
+  slo: true,
+  cache: {
+    enable: false,
+    ttl: 5 * 60 * 1000,
+    filter: []
+  },
+  fromAjax: {
+    header: 'x-client-ajax',
+    status: 418
+  }
 });
